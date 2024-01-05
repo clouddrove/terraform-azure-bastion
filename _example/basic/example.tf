07b32491602eeff792ed187002e7266af72bbcad
Pranay Deokar <pranay.deokar@clouddrove.com>
2024-01-06 01:48:11 +0530

    fix: Fixed tf-checks / basic-example

--- _example/basic/example.tf
+++ _example/basic/example.tf
@@ -6,14 +6,13 @@ provider "azurerm" {
 ## Bastion module call.
 ##-----------------------------------------------------------------------------
 module "bastion" {
-  depends_on           = [module.resource_group]
   source               = "./../../"
   name                 = "app"
   environment          = "test"
-  resource_group_name  = module.resource_group.resource_group_name
-  location             = module.resource_group.resource_group_location
-  virtual_network_name = module.vnet.vnet_name
-  subnet_id            = module.name_specific_subnet.specific_subnet_id[0]
+  resource_group_name  = "resource_group_name"
+  location             = "Central India"
+  virtual_network_name = "virtual_network"
+  subnet_id            = "<subnet_id>"
 
   ##-----------------------------------------------------------------------------
   ## enable diagnostic setting
