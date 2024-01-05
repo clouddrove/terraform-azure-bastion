958b69e9df53a07b00422a05e1bcca194fa9972d
Pranay Deokar <pranay.deokar@clouddrove.com>
2024-01-06 01:55:21 +0530

    fix: tf-checks / basic-example

--- _example/basic/example.tf
+++ _example/basic/example.tf
@@ -6,13 +6,12 @@ provider "azurerm" {
 ## Bastion module call.
 ##-----------------------------------------------------------------------------
 module "bastion" {
-  source               = "./../../"
-  name                 = "app"
-  environment          = "test"
-  resource_group_name  = "resource_group_name"
-  location             = "Central India"
-  virtual_network_name = "virtual_network"
-  subnet_id            = "<subnet_id>"
+  source              = "./../../"
+  name                = "app"
+  environment         = "test"
+  resource_group_name = "resource_group_name"
+  location            = "Central India"
+  subnet_id           = "<subnet_id>"
 
   ##-----------------------------------------------------------------------------
   ## enable diagnostic setting
