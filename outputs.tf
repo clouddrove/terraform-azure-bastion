15e3e53dcbe395669d3008ea8500d615a7436dc4
Rupal Sharma <rupal.sharma@clouddrove.com>
2024-02-19 17:55:39 +0530

    fix:change in count condition

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "dns_name" {
-  value       = azurerm_bastion_host.main[0].dns_name
+  value       = try(azurerm_bastion_host.main[0].dns_name, null)
   description = "Specifies the name of the bastion host"
 }
 
 output "id" {
-  value       = azurerm_bastion_host.main[0].id
+  value       = try(azurerm_bastion_host.main[0].id, null)
   description = "Specifies the resource id of the bastion host"
 }
